b05da60776a6a48a99f4ad802b8c07a921b0b75d
Tejashri Mandlik <161927572+tejashri4@users.noreply.github.com>
2024-12-02 12:25:29 +0530

    Day2
    I just built my first interactive code which takes inputs from the user! Day 2 of #Replit100DaysOfCode #100DaysOfCode. Join me on @Replit https://join.replit.com/python

---
+++ day2.py
@@ -0,0 +1,13 @@
+#python Day 2...
+
+print("Getting to know you!")
+
+yourName = input("What is your Name")
+Hungry = input("What is your favourite food?")
+music = input("What is your favorite music ?")
+location = input("Where do you live ?")
+print("you are " + yourName )
+print("You are probaly hungry for " + Hungry)
+print("and you are definitely getting your groove on to " + music)
+print("living in amazing "  + location)
+print (" Have a great day..!!")
